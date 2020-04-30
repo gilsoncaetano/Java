d4c3109764959f9a6332bd9a72ceeb33a521cd90
Gilson Caetano <53578134+gilsoncaetano@users.noreply.github.com>
2020-04-30 01:39:44 -0300

    Atividade Hello

--- Aula_01/Hello.java
+++ Aula_01/Hello.java
@@ -1,12 +1,11 @@
-/**
- * 
- */
+
 package cursoJava;
 
 /**
  * @author Gilson Caetano
  *  Hello World
  */
+
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
