2c193ffe7b4e9a6e2db25d9a939c0ba19c5e6442
Gilson Caetano <gilsonebcs@gmail.com>
2020-05-27 15:59:34 -0300

    Adicionando mais uma linha no código

--- Aula_01/Hello.java
+++ Aula_01/Hello.java
@@ -10,6 +10,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Gilson Caetano");
+		System.out.println("Caetano");
 	}
 
 }
--- TI-2019/src/Curso_Java/Hello.java
+++ TI-2019/src/Curso_Java/Hello.java
@@ -9,6 +9,8 @@ package Curso_Java;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Gilson Cetano");
+		System.out.println("Teste");
 
 	}
 
